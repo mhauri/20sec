0da1b18d3b57d03ce1fd15d18745fe5e3dfbf3ea
Marcel Hauri <marcel.hauri@gmail.com>
2021-07-06 09:39:45 +0200

    [task] add load more button

--- components/footer.js
+++ components/footer.js
@@ -3,12 +3,15 @@ import HeartIcon from "./heart";
 export default function Footer() {
   return (
     <footer className="footer">
-      <p className={"copyright"}>Erstellt mit <HeartIcon/> von <a href={"https://marcelhauri.ch"}>Marcel Hauri</a> | Source: <a href={"https://github.com/mhauri/20sec"}>GitHub</a></p>
+      <p className={"copyright"}>Erstellt mit <HeartIcon/> von <a href={"https://marcelhauri.ch"}>Marcel Hauri</a> |
+        Source: <a href={"https://github.com/mhauri/20sec"}>GitHub</a></p>
       <p className={"disclaimer"}>
-        <strong>Haftungsausschluss:</strong> <br />Ich bin nicht mit der TX Group oder einer ihrer Tochtergesellschaften oder angeschlossenen Unternehmen verbunden, assoziiert, autorisiert, befürwortet oder in irgendeiner Weise offiziell verbunden.
+        <strong>Haftungsausschluss:</strong> <br/>Ich bin nicht mit der TX Group oder einer ihrer Tochtergesellschaften
+        oder angeschlossenen Unternehmen verbunden, assoziiert, autorisiert, befürwortet oder in irgendeiner Weise
+        offiziell verbunden.
         Die offizielle Website der TX Group AG finden Sie unter <a href={"https://tx.group/"}>https://tx.group/</a>.
-        Die Namen 20 Minuten und TX Group AG sowie alle damit verbundenen Namen, Marken, Embleme und Bilder sind eingetragene Warenzeichen der jeweiligen Eigentümer.</p>
-
+        Die Namen 20 Minuten und TX Group AG sowie alle damit verbundenen Namen, Marken, Embleme und Bilder sind
+        eingetragene Warenzeichen der jeweiligen Eigentümer.</p>
     </footer>
   )
 }
--- components/search.js
+++ components/search.js
@@ -9,28 +9,39 @@ export default function Search() {
   const [query, setQuery] = useState('');
   const [search, setSearch] = useState('%20%20%20');
   const [data, setData] = useState({hits: []});
+  const [start, setStart] = useState(1);
+  const [isFetching, setIsFetching] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
 
   const onSubmit = useCallback((event) => {
     event.preventDefault();
   })
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios(
-        `https://feed-prod.unitycms.io/6/search?q=${search}`,
-      );
-      setData(result.data);
-    };
+  const fetchData = async () => {
+    setIsFetching(true);
+    const result = await axios({
+        method: 'GET',
+        url: 'https://feed-prod.unitycms.io/6/search',
+        params: {q: search, start: start}
+      }
+    );
+    setData(result.data);
+    setHasMore(result.data.nextpage !== 'undefined');
+    setStart(start + 20)
+    setIsFetching(false);
+  };
 
+  useEffect(() => {
     fetchData();
   }, [search]);
 
   return (
-    <div ref={searchRef}>
+    <div ref={searchRef} id={"top"}>
       <header className={"header"}>
         <form onSubmit={onSubmit}>
-          <h1><a href={"/"}><img src={"/apple-touch-icon.png"} className={"inline w-24"} alt={"20sec"}/></a> <br />Die fehlende 20 Minuten Suchfunktion</h1>
-          <br />
+          <h1><a href={"/"}><img src={"/apple-touch-icon.png"} className={"inline w-24"} alt={"20sec"}/></a> <br/>Die
+            fehlende 20 Minuten Suchfunktion</h1>
+          <br/>
           <input
             onChange={event => setQuery(event.target.value)}
             placeholder='Suche'
@@ -46,31 +57,40 @@ export default function Search() {
       </header>
       {data && data.content && data.content.length > 0 && (
         <div className={"list"}>
-          {data.content.map(({id, type, content}) => (
-            <div key={id} className={"item"}>
-              <div className={"item__content"}>
-                <a href={"https://20min.ch" + content.url}>
-                  <div className={'item__image'}>
-                    <img src={content.image.variants.big.src} alt={content.image.name}/>
-                  </div>
-                  <div className={"item__date"}>
-                    {moment(content.published).format('lll')}
-                  </div>
-                  <div className={"item__title"}>
-                    <h3>{content.title}</h3>
-                  </div>
-                  <div className={"item__lead"}>
-                    <p>{content.lead}</p>
-                  </div>
-                  <div className={"item__type"}>
-                    {type.slice(0, -1)}
-                  </div>
-                </a>
+          {data.content.map(({id, type, content}) => {
+            return (
+              <div key={id} className={"item"}>
+                <div className={"item__content"}>
+                  <a href={"https://20min.ch" + content.url}>
+                    <div className={'item__image'}>
+                      {content.image && <img src={content.image.variants.big.src} alt={content.image.name}/>}
+                    </div>
+                    <div className={"item__titleHeader"}>
+                      <h4>{content.titleHeader}</h4>
+                    </div>
+                    <div className={"item__title"}>
+                      <h3>{content.title}</h3>
+                    </div>
+                    <div className={"item__lead"}>
+                      <p>{content.lead}</p>
+                    </div>
+                    <div className={"item__date"}>
+                      {moment(content.published).format('lll')}
+                    </div>
+                    <div className={"item__type"}>
+                    <span
+                      className={type}>{type.replace('articles', 'Artikel').replace('videos', 'Video').replace('tickers', 'Ticker')}</span>
+                      {content.settings.breakingNews && <span className={"breaking"}>Breaking News</span>}
+                      {content.settings.live && <span className={"live"}>Live</span>}
+                    </div>
+                  </a>
+                </div>
               </div>
-            </div>
-          ))}
+            )
+          })}
         </div>
       )}
+      {hasMore && <a href={"#top"} onClick={() => fetchData()} className={"button__more"}>weiter</a>}
     </div>
   )
 }
